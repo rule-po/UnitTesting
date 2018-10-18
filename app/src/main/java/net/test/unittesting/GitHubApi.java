eda505feb95ab28725f62654b52c88f6a71a2a54
rule <ivan.jukic89@gmail.com>
2018-10-18 16:31:41 +0200

    Uploaded project

---
+++ app/src/main/java/net/test/unittesting/GitHubApi.java
@@ -0,0 +1,14 @@
+package net.test.unittesting;
+
+import net.test.unittesting.models.SearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface GitHubApi {
+    @Headers({"Accept: application/vnd.github.mercy-preview+json"})
+    @GET("search/repositories")
+    Call<SearchResponse> searchRepos(@Query("q") String term);
+}
---
+++ app/src/main/java/net/test/unittesting/models/SearchResponse.java
@@ -0,0 +1,32 @@
+package net.test.unittesting.models;
+
+import android.support.annotation.Nullable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SearchResponse {
+    @Expose
+    private Integer totalCount;
+    @SerializedName("incomplete_results")
+    @Expose
+    private Boolean incompleteResults;
+    @SerializedName("items")
+    @Expose
+    private List<SearchResult> searchResults = null;
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public Boolean getIncompleteResults() {
+        return incompleteResults;
+    }
+
+    @Nullable
+    public List<SearchResult> getSearchResults() {
+        return searchResults;
+    }
+}
